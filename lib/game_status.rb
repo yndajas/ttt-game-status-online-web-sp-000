e20a75e3fa64037252b48684b97ff4a0f020d68a
Ynda Jas <yndajas@gmail.com>
2020-06-30 02:58:59 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -28,8 +28,3 @@ def won?(board)
   end
 end
 
-    if board(combination[0]) == "X" && board(combination[1]) == "X" && board(combination[2]) == "X"
-      return combination
-    elsif board(combination[0]) == "O" && board(combination[1]) == "O" && board(combination[2]) == "O"
-      return combination
-    end
