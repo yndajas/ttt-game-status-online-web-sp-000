e5d95cb7b9528c4b0fe1de822d6b2f2132e30da1
Ynda Jas <yndajas@gmail.com>
2020-06-30 02:39:34 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -20,5 +20,8 @@ def won?(board)
   WIN_COMBINATIONS.each do |combination|
     if board(combination[0]) == "X" && board(combination[1]) == "X" && board(combination[2]) == "X"
       return combination
-    elsif board(combination[0]) == "O" && board(combination[1]) == "O" && board(combination[2]) == "X"
+    elsif board(combination[0]) == "O" && board(combination[1]) == "O" && board(combination[2]) == "O"
+      return combination
+    end
+  end
 end
